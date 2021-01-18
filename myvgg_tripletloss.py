20e6c5f6cba3408964ea0a0d2d50867cbefa9ff0
soniamaitieo <tieo.soniamai@gmail.com>
2021-01-18 17:18:24 +0100

    add own TOP-N accuracy - callout - work

--- myvgg_tripletloss.py
+++ myvgg_tripletloss.py
@@ -17,13 +17,16 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import datetime
 import sys 
+from operator import itemgetter 
+from keras.callbacks import Callback
+import scipy.spatial
 
 #--- GLOBAL VAR ---#
 inputShape = (224, 224)
 #num_classes = 1
 NOFREEZE = 2
 BATCH_SIZE = 128
-NB_EPOCHS = 30
+NB_EPOCHS = 10
 #NB_EPOCHS2 = 5
 LR = 0.001
 opt = tf.keras.optimizers.Adam(learning_rate= LR)
@@ -31,10 +34,15 @@ opt = tf.keras.optimizers.Adam(learning_rate= LR)
 
 #--- DATASET DIR ---#
 
-#train_dir = '/media/sonia/DATA/CFDVersion2.0.3/CFD2.0.3Images'
 #train_dir = '/media/sonia/DATA/CASIA-WebFace_SUB'
-train_dir = "/media/sonia/DATA/casia_sub_sub/images"
-val_dir = "/media/sonia/DATA/CFDVersion2.0.3/CFD2.0.3Images"
+#train_dir = "/media/sonia/DATA/casia_sub_sub/images"
+train_dir = '/media/sonia/DATA/CASIA_SUB_TRAIN/images'
+val_dir = "/media/sonia/DATA/CASIA_SUB_VAL"
+test_dir = "/media/sonia/DATA/CFDVersion2.0.3/CFD2.0.3Images"
+
+#train_dir = "/media/sonia/DATA/CASIA_minitrain"
+#val_dir = "/media/sonia/DATA/CASIA_mini"
+
 #nb img/ind
 #find . -maxdepth 1 -mindepth 1 -type d | while read dir; do   printf "%-25.25s : " "$dir";   find "$dir" -type f | wc -l; done
     
@@ -44,16 +52,16 @@ today =  datetime.date.today()
 now = datetime.datetime.now()
 
 
-#create_folder for results
+#---create_folder for results
 todaystr = today.isoformat() + '-' + str(now.hour) + '-' + str(now.minute)
 os.mkdir("results/"   +  todaystr )
 
-#create log file to save all steps and outputs
+#---create log file to save all steps and outputs
 log_file_path = "results/" + todaystr + "/log_file.txt"
 sys.stdout = open(log_file_path, 'w')
 
 
-#Write a memo file with parameters used for CNN
+#---Write a memo file with parameters used for CNN
 
 #OPTIM2 = "keras.optimizers.SGD(lr=0.0001, momentum=0.9, decay=0.0, nesterov=True)"
 params_file_path =  "results/" + todaystr + "/params.txt"
@@ -67,7 +75,7 @@ f.write("optim1_LR:" + str(LR) + "\n")
 #f.write("optim2:" + OPTIM2  + "\n")
 f.write("no freezen layer:" + str(NOFREEZE) + "\n")
 #f.write("Img_preprocessing" + "rescale=1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2)")
-f.write("Img_preprocessing:" + "rescale=1./255")
+f.write("Img_preprocessing:" + "rescale=1./255, horizontal_flip=False, rotation_range=20,width_shift_range=0.05, height_shift_range=0.05,brightness_range=[0.8,1.2], shear_range=0.1,zoom_range=[0.85,1.15]")
 #f.write("val:" + str(0.20) + "\n")
 #f.write("early_stop:" + str(10) + "\n")
 #f.write("reducelr0.1:" + str(5) + "\n")
@@ -77,6 +85,64 @@ f.write("Img_preprocessing:" + "rescale=1./255")
 
 f.close()
 
+
+#--- Functions to calculate our own accuracy ---#
+
+#src: https://plmlab.math.cnrs.fr/vmiele/animal-reid/-/blob/master/deepMetricLearning.py
+
+def distance_l2(y1,y2):
+     return scipy.spatial.distance.euclidean(y1,y2)**2 ## square of euclidian distance, as in training
+ 
+    
+def compute_distance(model,x):
+     N = len(x.filenames)
+     distance_array = np.zeros((N,N))
+     vec = model.predict(valid_generator,batch_size=16,verbose=1)
+     for i in range(N):
+          for j in range(N):
+               d=distance_l2(vec[i][:],vec[j][:])
+               distance_array[i,j]=d
+     return distance_array
+
+
+def calc_TOPacc(distance_array, valid_generator, TOP = 3):
+    TOPacc = []
+    labels = valid_generator.filenames
+    for i in range(len(distance_array)):
+        #i = 1
+        TOP = 3 + 1
+        distance_i = distance_array[i,:]
+        original = labels[i]
+        idx = np.argpartition(distance_i, TOP)
+        #This returns the k-smallest values. Note that these may not be in sorted order.
+        L= list(idx[:TOP])
+        if i in L: L.remove(i)
+        #L = list(distance_i[idx[:TOP]])
+        #print(itemgetter(*L)(labels))
+        if original.split('/')[0] in [i.split('/', 1)[0] for i in itemgetter(*L)(labels)]:
+            TOPacc.append(1)
+        else:
+            TOPacc.append(0)
+    return(sum(TOPacc)/len(TOPacc))
+    
+
+## test compared to train images
+class TopX(Callback):
+     def __init__(self, x):
+          self.x = valid_generator
+          self.topX = []
+          
+     def on_train_begin(self, logs={}):
+          self.topX = []
+          
+     def on_epoch_end(self, epoch, logs={}):
+        TOP = 3
+        distance_array = compute_distance(self.model,self.x)
+        #print(self.x.filenames)
+        topX = calc_TOPacc(distance_array, self.x, TOP = TOP)
+        #print("Top {TOP} - Accuracy(%):   ", round(topX*100,1))
+        print(' Top {} - Accuracy {} %'.format(TOP, round(topX*100,1)))
+        #print(topX)
 
 with tf.device('/gpu:0'):
     vggface = tf.keras.models.Sequential()
@@ -118,13 +184,25 @@ with tf.device('/gpu:0'):
     vggface.compile(optimizer=opt, loss=tfa.losses.triplet_semihard_loss ,metrics=['accuracy'])
     #vggface.compile( optimizer=opt, loss = tfa.losses.TripletHardLoss())
     #vggface.compile(optimizer=opt, loss=tfa.losses.TripletSemiHardLoss(),metrics=['accuracy'])
-    data_gen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
+    #data_gen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
     #train_datagen = ImageDataGenerator(rescale=1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2)
-    data_gen_valid = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255. ,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2)
+    data_gen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255. ,
+                                                                     horizontal_flip=False,
+                                                                     rotation_range=20,
+                                                                     width_shift_range=0.05,
+                                                                     height_shift_range=0.05,
+                                                                     brightness_range=[0.8,1.2], 
+                                                                     shear_range=0.1,
+                                                                     zoom_range=[0.85,1.15])
+    data_gen_valid = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
+    data_gen_test = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
     train_generator = data_gen_train.flow_from_directory(train_dir, target_size=inputShape, batch_size=BATCH_SIZE, class_mode="sparse")
     valid_generator = data_gen_valid.flow_from_directory(val_dir, target_size=(224,224), batch_size=BATCH_SIZE, class_mode="sparse")
-    history = vggface.fit(train_generator, epochs=NB_EPOCHS , batch_size = BATCH_SIZE)
-    results = vggface.predict(valid_generator)
+    #history = vggface.fit(train_generator, epochs=NB_EPOCHS , batch_size = BATCH_SIZE)
+    topx = TopX(valid_generator)    
+    history = vggface.fit_generator(train_generator,epochs=NB_EPOCHS ,callbacks=[topx])
+    test_generator = data_gen_valid.flow_from_directory(test_dir, target_size=(224,224), batch_size=BATCH_SIZE, class_mode="sparse")
+    results = vggface.predict(test_generator)
     
 np.savetxt("results/" + todaystr + "/CFDvecs.tsv", results, delimiter='\t')
 
@@ -150,11 +228,6 @@ pdftest=var.pdf(results)
 log_pdftest= np.log(pdftest)
 np.savetxt("results/" + todaystr + "/CFD_LL.tsv", log_pdftest, delimiter='\t')
 
-
-def gaussian_mle(data):                                                                                                                                                                               
-    mu = data.mean(axis=0)                                                                                                                                                                            
-    var = (data-mu).T @ (data-mu) / data.shape[0] #  this is slightly suboptimal, but instructive
-    return mu, var 
 
 
 
